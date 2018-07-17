a33050c545cd479704e74ef3480cee26a7fdb377
Sophia Di <backwardsxmarathon@gmail.com>
2018-07-17 00:21:20 -0400

    Add dark sky api call GET route to server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,43 @@
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+const port = process.env.PORT || 5000;
+const darkSkyApiKey = process.env.DARK_SKY_API_KEY;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/api/currentWeather/:lat/:lng', (req, res) => {
+  let { lat, lng } = req.params;
+  let darkSkyUrl = `https://api.darksky.net/forecast/${darkSkyApiKey}/${lat},${lng}`;
+  axios.get(darkSkyUrl).then((wxRes) => {
+    var { sunriseTime, sunsetTime } = wxRes.data.daily.data[0];
+    var cur = wxRes.data.currently;
+    res.send({
+      wx: {
+        temp: cur.temperature,
+        dewPoint: cur.dewPoint,
+        pressure: cur.pressure,
+        windBearing: cur.windBearing,
+        windSpeed: cur.windSpeed
+      },
+      // time variables in SECONDS since 1 January 1970
+      time: cur.time,
+      sunrise: sunriseTime,
+      sunset: sunsetTime
+    });
+  });
 });
+
+
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  app.use(static(join(__dirname, '..', 'client', 'build')));
   // Handle React routing, return all requests to React app
   app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
